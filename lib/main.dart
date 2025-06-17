23e62d48482a9ca97eff6383bd2d6e433ec72c08
Euan Douglas <euan9douglas@hotmail.co.uk>
2025-06-17 19:24:13 +0100

    bear bones of app. selection system working :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,27 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +33,216 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => ActivityEditor(title: 'Editor'))),
+              child: Text('Activity Editor'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => Rooms(title: 'Rooms'))),
+              child: Text('Rooms'),
             ),
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+    );
+  }
+}
+
+typedef ActivityEntry = DropdownMenuEntry<Activity>;
+
+enum Activity {
+  a('Option A', 'assets/placeholder.jpg'),
+  b('Option B', 'assets/b.jpg'),
+  c('Option C', 'assets/c.jpg');
+
+  const Activity(this.text, this.image);
+  final String text;
+  final String image;
+
+  static final List<ActivityEntry> entries = UnmodifiableListView<ActivityEntry>(
+    values.map<ActivityEntry>(
+      (Activity activity) => ActivityEntry(value: activity, label: activity.text),
+    ),
+  );
+}
+
+class ActivityEditor extends StatefulWidget {
+  ActivityEditor({super.key, required this.title});
+
+  final String title;
+  final List<DropdownMenuItem> activities = [];
+
+  @override
+  State<ActivityEditor> createState() => _ActivityEditorState();
+}
+
+class _ActivityEditorState extends State<ActivityEditor> {
+
+  Activity morning = Activity.a;
+  Activity lunch = Activity.a;
+  Activity afternoon = Activity.a;
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Select Activities:'),
+            Row(
+              children: [
+                Spacer(),
+                Text('Morning'),
+                Spacer(),
+                Text('Midday'),
+                Spacer(),
+                Text('Afternoon'),
+                Spacer(),
+              ],
+            ),
+            Row(
+              children: [
+                Spacer(),
+                DropdownMenu<Activity>(
+                  initialSelection: Activity.a,
+                  dropdownMenuEntries: Activity.entries, 
+                  onSelected: (object) => morning=object!
+                ),
+                Spacer(),
+                DropdownMenu<Activity>(
+                  initialSelection: Activity.a,
+                  dropdownMenuEntries: Activity.entries, 
+                  onSelected: (object) => lunch=object!
+                ),
+                Spacer(),
+                DropdownMenu<Activity>(
+                  initialSelection: Activity.a,
+                  dropdownMenuEntries: Activity.entries, 
+                  onSelected: (object) => afternoon=object!
+                ),
+                Spacer(),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => ActivityDisplay(title: 'ActivityDisplay', morning: morning, lunch: lunch, afternoon: afternoon,))),
+              // onPressed: () => print('${morning.image}, ${lunch.image}, ${afternoon.image}'),
+              child: Text('Run Selection'))
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+class ActivityDisplay extends StatefulWidget {
+  const ActivityDisplay({super.key, required this.title, required this.morning, required this.lunch, required this.afternoon});
+
+  final String title;
+  final Activity morning;
+  final Activity lunch;
+  final Activity afternoon;
+
+  @override
+  State<ActivityDisplay> createState() => _ActivityDisplayState();
+}
+
+class _ActivityDisplayState extends State<ActivityDisplay> {
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title),),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Spacer(),
+            Column(
+              children: [
+                Image.asset(widget.morning.image),
+                Text(widget.morning.text),
+              ],
+            ),
+            Spacer(),
+            Column(
+              children: [
+                Image.asset(widget.lunch.image),
+                Text(widget.lunch.text),
+              ],
+            ),
+            Spacer(),
+            Column(
+              children: [
+                Image.asset(widget.afternoon.image),
+                Text(widget.afternoon.text),
+              ],
+            ),
+            Spacer(),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+class Rooms extends StatefulWidget {
+  const Rooms({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Rooms> createState() => _RoomsState();
+}
+
+class _RoomsState extends State<Rooms> {
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Rooms screen'),
+          ],
+        ),
+      ),
+
     );
   }
 }
